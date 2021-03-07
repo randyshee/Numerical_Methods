d8ca1c6cdb25a6d68d593fee5f557f83879453fe
randyshee <randyshee@berekley.edu>
2021-03-07 13:04:56 +0800

    Add method for Simpson's 3/8 rule

--- integration.py
+++ integration.py
@@ -60,8 +60,37 @@ def simpson_1_3_rule(fn, range_x, n=6):
 		   4*sum([fn(l + i*h) for i in range(1, n, 2)]) + \
 		   2*sum([fn(l + i*h) for i in range(2, n, 2)]))
 
-def simpson_3_8_rule():
-	pass
+def simpson_3_8_rule(fn, range_x, n=6):
+	"""
+	This functino uses Simpson's 3/8 rule to calculate the area 
+	under a curve `fn` with `n` divisions  within a range of x 
+	values `range_x`. The Simpson's 3/8 rule uses weighing factors 
+	to  minimize the error from  trapezoidal method with the formula
+	for the first three strips being A=3*h*[f(x0)+3*f(x1)+3*f(x2)+f(x3)]/8.
+
+	Args:
+	    fn : a user defined or lambda function that returns a float
+	    range_x ([float, float]) : lower and upper limit of x values
+	    n (int) : the number of divisions (has to multiple of three)
+
+	Returns:
+	    float : the approximated integrated value at a given range
+
+	>>> fn = lambda x: x*np.sin(x)
+	>>> range_x = [0, np.pi/2] # analytical result = 1
+	>>> np.around(simpson_3_8_rule(fn, range_x, n=6), 6)
+	0.999819
+	>>> np.around(simpson_3_8_rule(fn, range_x, n=9), 6)
+	0.999965
+	>>> np.around(simpson_3_8_rule(fn, range_x, n=99), 6)
+	1.0
+	"""
+	assert n%3 == 0, 'The number of divisions has to be multiple of 3!'
+	l, r = range_x[0], range_x[1]
+	h = (r - l)/n
+	return 3*h/8*(fn(l) + fn(r) + \
+		   3*sum([fn(l + i*h) + fn(l + (i + 1)*h) for i in range(1, n, 3)]) \
+		   + 2*sum([fn(l + i*h) for i in range(3, n, 3)]))
 
 def double_integration():
 	pass
